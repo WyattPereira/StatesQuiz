0f22e9bcd0289237f7af127cfd0f1ffaf35e59d5
WyattPereira <wyattpereira04@gmail.com>
2021-07-14 11:31:56 -0230

    Brand New Project!

---
+++ main.py
@@ -0,0 +1,12 @@
+from functions import *
+# US States Quiz
+
+print("Welcome To The U.S. States Quiz!\n"
+      "You will be tested on your knowledge of states, their Capital City"
+      " and the Year in which they became a state!")
+print("\nLet's Begin!\n")
+us_states()
+
+
+
+
